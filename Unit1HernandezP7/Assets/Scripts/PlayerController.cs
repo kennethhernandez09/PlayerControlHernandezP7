08718a32b4a0328227258173b5cbcd6a156494d9
Kenneth Hernandez <145072274+kennethhernandez09@users.noreply.github.com>
2024-03-19 00:23:34 -0700

    finish game
    finished the game have to do challenge 1 tho

---
+++ Unit1HernandezP7/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 20.0f;
+    private float turnspeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    public Camera mainCamera; 
+    public Camera hoodCamera; 
+    public KeyCode switchKey;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnspeed * horizontalInput * Time.deltaTime);
+
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
+    }
+}
